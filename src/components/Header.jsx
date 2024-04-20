3fd0f956023cb53327ec790744b8d19fbe2f2e25
George Kapanadze <52297225+kapana1004@users.noreply.github.com>
2024-04-20 23:44:34 +0400

    add onMousover to andle arrows changing

---
+++ src/components/Header.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import Navigation from "./Navigation";
+import SearchImg from "../../public/assets/Search.jpeg";
+import DotsImg from "../../public/assets/Dots.png";
+import ImageImg from "../../public/assets/ImageIcon.png";
+import ArrowDownImg from "../../public/assets/ArrowDown.png";
+import ArrowUpImg from "../../public/assets/ArrowUp.png";
+
+export default function Header() {
+  const [arrow, setArrow] = useState(false);
+  const handleArrow = () => {
+    setArrow(!arrow);
+  };
+  return (
+    <div className=" ">
+      <div className="header min-h-[500px]">
+        <Navigation />
+        <div className=" flex max-w-[630px] mx-auto pt-[50px] flex-col gap-y-[48px]">
+          <p className=" text-white text-[33px]">
+            The best free stock photos, royalty free images & videos shared by
+            creators.
+          </p>
+          <div className=" w-[100%] h-[50px] flex flex-row bg-white pl-[5px] pt-[5px] pb-[5px] rounded-[10px]">
+            <button className=" bg-red-500 w-[130px] pl-[15px] pr-[15px] border-[#bfbfbf] hover:opacity-75 rounded-[5px] flex flex-row items-center justify-between">
+              <img
+                className=" w-[16px] h-[16px] mt-[3px]"
+                src={ImageImg}
+                alt=""
+              />
+              <span className=" text-[16px] font-semibold font">photos</span>
+              <img
+                onMouseOver={handleArrow}
+                className=" w-[16px] h-[16px] mt-[3px]"
+                src={arrow ? ArrowUpImg : ArrowDownImg}
+                alt=""
+              />
+            </button>
+            <input
+              className=" w-[430px] text-[18px] pt-[15px] pr-[4px] pb-[15px] pl-[20px]"
+              type="text"
+              placeholder="Search for free photos"
+            />
+            <button className=" w-[55px] bg-white flex justify-center items-center hover:opacity-70">
+              {" "}
+              <img
+                className=" w-[25px] h-[25px]"
+                src={SearchImg}
+                alt="search"
+              />
+            </button>
+          </div>
+          <div className=" flex flex-row">
+            <span className=" text-[#bfbfbf] pr-[10px] text-[16px] leading-[26px]">
+              Trending:{" "}
+            </span>{" "}
+            <span className=" text-white text-[16px] pr-[5px] cursor-pointer hover:text-emerald-200">
+              plant,{" "}
+            </span>
+            <span className=" text-[16px] text-white pr-[5px] cursor-pointer hover:text-emerald-200">
+              motivational quotes,{" "}
+            </span>{" "}
+            <span className=" text-[16px] text-white pr-[5px] cursor-pointer hover:text-emerald-200">
+              sustainability,{" "}
+            </span>
+            <span className=" text-white text-[16px] pr-[5px] cursor-pointer hover:text-emerald-200">
+              spa,{" "}
+            </span>
+            <span className=" text-[16px] text-white pr-[10px] cursor-pointer hover:text-emerald-200">
+              stock video{" "}
+            </span>
+            <div className="w-[24px] h-[24px] bg-slate-200 cursor-pointer border-[2px] hover:border-[cyan] rounded-full">
+              {" "}
+              <img className=" rounded-[50px]" src={DotsImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import LogoImg from "../../public/assets/Logo.jpeg";
+export default function Navigation() {
+  return (
+    <nav className=" flex justify-between pl-[60px] pr-[60px] pt-[30px] w-[100%]">
+      <div className=" flex w-[120px] h-[50px] justify-between items-center cursor-pointer hover:opacity-50">
+        <img
+          className=" w-[60px] h-[60px] rounded-[10px]"
+          src={LogoImg}
+          alt="logo"
+        />
+        <span className=" text-white text-[20px]">Pexel</span>
+      </div>
+      <button className=" w-[100px] h-[50px] bg-white rounded-[10px] hover:bg-emerald-200">
+        Join
+      </button>
+    </nav>
+  );
+}
